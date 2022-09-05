22f615c648db75ffda208c936c6bd44e437d4ef6
cumany <42957010+cumany@users.noreply.github.com>
2022-09-05 15:03:52 +0800

    1. Fixed the problem of inserting the internal link cursor positioning 2. added a new style tiny 3. added new position option following, which means cmenu will follow the cursor.
    1. Fixed the problem of inserting the internal link cursor positioning
    2. added a new style tiny
    3. added new position option following, which means cmenu will follow the cursor.

--- src/modals/cMenuModal.ts
+++ src/modals/cMenuModal.ts
@@ -11,7 +11,18 @@ export function selfDestruct() {
     cMenuModalBar.remove();
   }
 }
+const getcoords = ( editor:any ) => {
+  const cursor = editor.getCursor("from");
 
+  let coords;
+  if (editor.cursorCoords) coords = editor.cursorCoords(true, "window");
+  else if (editor.coordsAtPos) {
+      const offset = editor.posToOffset(cursor);
+      coords = (editor).cm.coordsAtPos?.(offset) ?? (editor).coordsAtPos(offset);
+  } else return;
+
+  return coords;
+};
 export function cMenuPopover(app: App, settings: cMenuSettings): void {
   function createMenu() {
     const generateMenu = () => {
@@ -25,53 +36,114 @@ export function cMenuPopover(app: App, settings: cMenuSettings): void {
         );
       }
       cMenu.setAttribute("id", "cMenuModalBar");
-      settings.aestheticStyle == "default"
-        ? cMenu.addClass("cMenuDefaultAesthetic")
-        : cMenu.addClass("cMenuGlassAesthetic");
+      if( settings.aestheticStyle == "default")
+            {
+              cMenu.addClass("cMenuDefaultAesthetic");
+              cMenu.removeClass("cMenuTinyAesthetic");
+              cMenu.removeClass("cMenuGlassAesthetic");
+            } else if( settings.aestheticStyle == "tiny")
+            {
+              cMenu.addClass("cMenuTinyAesthetic");
+              cMenu.removeClass("cMenuDefaultAesthetic");
+              cMenu.removeClass("cMenuGlassAesthetic");
+            }else{
+              cMenu.addClass("cMenuGlassAesthetic");
+              cMenu.removeClass("cMenuTinyAesthetic");
+              cMenu.removeClass("cMenuDefaultAesthetic");
+            }
+            
       settings.appendMethod == "workspace"
         ? document.body
             .querySelector(".mod-vertical.mod-root")
             .insertAdjacentElement("afterbegin", cMenu)
         : document.body.appendChild(cMenu);
-      settings.menuCommands.forEach((item: Command) => {
-        var button = new ButtonComponent(cMenu);
-        button
-          .setIcon(item.icon)
-          .setClass("cMenuCommandItem")
-          .setTooltip(item.name)
-          .onClick(() => {
-            //@ts-ignore
-            app.commands.executeCommandById(item.id);
-          });
-      });
-      document.addEventListener("selectionchange", function (event) {
-        setTimeout(async () => {
-          const activeLeaf = app.workspace.getActiveViewOfType(MarkdownView);
-          if (activeLeaf) {
-            cMenu.style.visibility = activeLeaf.editor.somethingSelected() ? "visible" : "hidden";
-            const selection = window.getSelection();
-            // getCientRects returns all the positioning information we need
-            const selectionContainer = selection.getRangeAt(0).commonAncestorContainer as HTMLElement;
-            const rect = selectionContainer.getClientRects()[0]
 
-            cMenu.style.height = `${cMenu.offsetHeight}px`;
-            // TODO: parents are different, position is error.
-            cMenu.style.left = `${rect.right - cMenu.offsetWidth * 1.5}px`;
-            if (rect.bottom + cMenu.offsetHeight < document.body.clientHeight) {
-              cMenu.style.top = `${rect.bottom}px`;
-            } else {
-              cMenu.style.top = `${rect.top - cMenu.offsetHeight * 1.5}px`;
-            }
-            // after cancel select
-            let timmer = setInterval(() => {
-              if (!activeLeaf.editor.somethingSelected()) {
-                cMenu.style.visibility = "hidden";
-                clearInterval(timmer);
-              };
-            }, 100);
+        function followingbar (_event: any) {
+
+          let activeLeaf =app.workspace.activeLeaf
+          if (activeLeaf.getViewState().type === "empty") {
+            return;
           }
-        }, 10);
-      })
+          //console.log(activeLeaf.getViewState().state.mode)
+          if (activeLeaf.getViewState().state.mode === "source") {
+         
+            
+            let editor = activeLeaf.view.sourceMode.cmEditor
+
+            let coords=getcoords(editor);
+
+            let cMenuModalBar = document.getElementById("cMenuModalBar");
+            if(cMenuModalBar){ 
+              if(activeLeaf.getViewState().state.mode === 'preview'){
+ 
+                  cMenuModalBar.style.visibility = "hidden";
+                  return;
+              }
+              var selection = editor.somethingSelected()
+              if(settings.positionStyle == "following")
+              selection?cMenuModalBar.style.visibility = "visible":   cMenuModalBar.style.visibility = "hidden";
+
+              let  ElementCount = cMenuModalBar.childElementCount;
+              if(ElementCount)
+              {       ElementCount >= 40
+                ? cMenuModalBar.addClass("cMenuGrid")
+                : cMenuModalBar.addClass("cMenuFlex");
+              }
+              else
+              {
+                ElementCount=0;
+              }
+              let  cMenuRows =  settings.cMenuNumRows  ;
+              let cmheight =  Math.ceil(ElementCount / cMenuRows)   ;
+           
+
+              cMenu.style.height = 40 * cmheight   + "px";
+              if( settings.aestheticStyle == "tiny")
+              {
+               cMenu.style.height = 25 * cmheight   + "px";
+              }
+              var rleftwidth =  document.getElementsByClassName('side-dock-ribbon mod-left')[0]?.clientWidth??0 ;
+               
+              var leftwidth =  document.getElementsByClassName('mod-left-split')[0]?.clientWidth??0 ;
+          
+              var barwidth = document.getElementById("cMenuModalBar").offsetWidth;
+              var barHeight = document.getElementById("cMenuModalBar").offsetHeight;
+
+              var bodywidth=document.body.offsetWidth;
+              /*添加判断边界 by cuman*/
+              cMenu.style.top=coords.top-barHeight -30 + "px";
+              cMenu.style.left=coords.left - leftwidth -rleftwidth +20 +"px";
+
+              if((coords.left+ barwidth+15) >bodywidth)
+              cMenu.style.left=coords.left - leftwidth -rleftwidth - barwidth/1.5 -40+"px";
+      
+          
+            }    
+          }  
+        }
+        
+        if(settings.positionStyle == "following")
+        {
+          var scope = document.querySelector("body");
+          scope.addEventListener("mouseup", followingbar, false);
+
+        }
+        settings.menuCommands.forEach((item) => {
+          var button = new ButtonComponent(cMenu);                
+          button
+              .setIcon(item.icon)
+              .setClass("cMenuCommandItem")
+              .setTooltip(item.name)
+              .onClick(() => {
+              //@ts-ignore
+              app.commands.executeCommandById(item.id);
+              settings.cMenuVisibility == false
+              settings.positionStyle == "following"
+              ? ( cMenu.style.visibility = "hidden")
+              : (cMenu.style.visibility = "visible");
+          });                
+        });
+    
     };
     let Markdown = app.workspace.getActiveViewOfType(MarkdownView);
     if (Markdown) {
